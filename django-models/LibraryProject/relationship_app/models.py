e211e0e9b7f6bcf98f45c2818cb69796042427dd
otwjunior <jamesthebark@proton.me>
2025-07-24 13:51:56 +0300

    add human readable capability for each model

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -4,14 +4,26 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=30)
     author =models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    def __str__(self):
+        return f"{self.title} by {self.author.name}"
 
 class Library(models.Model):
     name = models.CharField(max_length=20)
     books = models.ManyToManyField(Book)
 
+    def __str__(self):
+        return f"{self.name} has {self.book.title}"
+
 class Librarian(models.Model):
     name = models.CharField(max_length=20)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} serves at {self.library}"
