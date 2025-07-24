c7092619a50c6004fd3cfd6e06aaf0a602a3ee1f
otwjunior <jamesthebark@proton.me>
2025-07-24 13:59:51 +0300

    improved query for books writen by specifiedd author

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -7,9 +7,13 @@ django.setup()
 from relationship_app.models import Author,Book,Library, Librarian
 
 def query_books_by_author(author_name):
-    books = Book.objects.filter(author__name=author_name)
-    for book in books:
-        print(book.title)
+    try:
+        autho = Author.objects.get(name=author_name)
+        books = Book.objects.filter(author=author)
+        for book in books:
+            print(book.title)
+    except Author.DoesNotExist:
+        print(f"no author with the name '{author_name}'")
 
 def list_books_in_library(library_name):
     try:
