3ff7c10821a229b5be97ced091639aeb716dfd7e
otwjunior <jamesthebark@proton.me>
2025-07-26 22:17:34 +0300

    add inline templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,20 +1,13 @@
 from django.urls import path
-from .views import (
-   list_books,
-   LibraryDetailView, 
-   CustomLoginView,
-   RegisterView,
-   CustomLogoutView,
-   home_view
-)
+from . import views
 #from .views import HelloVi
 urlpatterns = [
    path('books/', list_books, name= 'book-list'),
    path('listbooks/<str:name>/', LibraryDetailView.as_view(), name = 'available-books' ),
    
-   path('login/', CustomLoginView.as_view(), name = 'login'),
-   path('logout/', CustomLogoutView.as_view(), name ='logout'),
-   path('register/', RegisterView.as_view(), name = 'register'),
+   path('login/', CustomLoginView.as_view(template_name='relationship_app/login.html'), name = 'login'),
+   path('logout/', CustomLogoutView.as_view(template_name='relationship_app/logout.html'), name ='logout'),
+   path('register/', views.RegisterView.as_view(), name = 'register'),
    path('', home_view, name='home'),
 
 ]
