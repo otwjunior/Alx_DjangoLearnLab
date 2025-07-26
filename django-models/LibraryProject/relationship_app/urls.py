7154fc4d4efd1f8e91ca425de6bba0c1ce3e5905
otwjunior <jamesthebark@proton.me>
2025-07-26 12:20:08 +0300

    alter url import

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LibraryDetailView, list_books
+from .views import list_books, LibraryDetailView
 #from .views import HelloView
 urlpatterns = [
    path('books/', list_books, name= 'book-list'),
