1c0cee3b5816ef909e3ef414885df668b6d1799b
otwjunior <jamesthebark@proton.me>
2025-07-26 18:30:01 +0300

    login and logout

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Redirect after login
+LOGIN_REDIRECT_URL = 'home' #page diplayed after login
+#Redirect after logout
+LOGOUT_REDIRECT_URL = 'login'
+LOGIN_URL = 'login'
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Library
 from .models import Book
+from django.contrib.auth.views import  LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.detail import DetailView 
-# function based view
+from django.views import View
+from django.contrib.auth.decorators import login_required
 def list_books(request):
     books = Book.objects.all()
     return render (request, 'relationship_app/list_books.html',{'books': books})
@@ -17,6 +20,26 @@ class LibraryDetailView(DetailView):
     slug_field = 'name' # field in model
     slug_url_kwarg = 'name' # keyword argument from URL
 
-     
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
 
+class CustomLogoutView(LogoutView):
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+    
 
+class RegisterView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'relationship_app/register.html', {'form': form})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login') # after registration go to login page
+        return render(request, 'relationship_app/register.html', {'form': form})
+
+@login_required
+def home_view(request):
+    return render (request, 'relationship_app/home.html')
