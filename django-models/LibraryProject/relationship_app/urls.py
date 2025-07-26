986fa111dc62b60b89faaae61e3f8177d312f780
otwjunior <jamesthebark@proton.me>
2025-07-26 12:14:15 +0300

    change url pattern

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import LibraryDetailView, list_books
 #from .views import HelloView
 urlpatterns = [
-   path('books/', book_list_view, name= 'book-list'),
+   path('books/', list_books, name= 'book-list'),
    path('listbooks/<str:name>/', LibraryDetailView.as_view(), name = 'available-books' ),
    
 ]
