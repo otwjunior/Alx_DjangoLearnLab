80ed8a354ce1bbaf4791fa58fe9bb336564b514e
otwjunior <jamesthebark@proton.me>
2025-07-26 22:10:59 +0300

    typo fix

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, CustomLoginView, RegisterView, CustomLogoutView, home_view
+from .views import (
+   list_books,
+   LibraryDetailView, 
+   CustomLoginView,
+   RegisterView,
+   CustomLogoutView,
+   home_view
+)
 #from .views import HelloVi
 urlpatterns = [
    path('books/', list_books, name= 'book-list'),
    path('listbooks/<str:name>/', LibraryDetailView.as_view(), name = 'available-books' ),
+   
    path('login/', CustomLoginView.as_view(), name = 'login'),
    path('logout/', CustomLogoutView.as_view(), name ='logout'),
    path('register/', RegisterView.as_view(), name = 'register'),
