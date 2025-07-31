a37825606360d8a30138f78254ec30cc5fb7f9a7
otwjunior <jamesthebark@proton.me>
2025-07-31 15:58:38 +0300

    permission

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
-class  Book(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100) 
     publication_year = models.IntegerField()
