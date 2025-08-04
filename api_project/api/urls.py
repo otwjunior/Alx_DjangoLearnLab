8aa040b9a7e538c588cce2d03f7282de1c31facd
otwjunior <jamesthebark@proton.me>
2025-08-04 16:04:28 +0300

    add authetication

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from .views import BookList
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
+"""token retrivval endpoint"""
+from rest_framework.authtoken.views import obtain_auth_token
 
 #settup routers
 router = DefaultRouter()
@@ -10,5 +12,6 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
     path ('', include(router.urls)), #viewset routes here
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'), #token endpoint
 ]
 
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,6 +4,9 @@ from .models import Book
 from .serializers import BookSerializer
 """using viewset"""
 from rest_framework import viewsets
+"""add permission to book views"""
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 # view to list books
 class BookList(generics.ListAPIView):
@@ -13,3 +16,5 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes=[IsAuthenticated] #user need tobe auth to view books
+    authentication_classes = [TokenAuthentication] # user need tokens to authenticate
