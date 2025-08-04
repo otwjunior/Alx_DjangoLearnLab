15433943c0b7deb83284f45aba878461898d7ca9
otwjunior <jamesthebark@proton.me>
2025-08-04 10:22:08 +0300

    api endpoint

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
-from  .serializers import BookSerializer
+from .serializers import BookSerializer
 # view to list books
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
