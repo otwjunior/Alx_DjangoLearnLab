c9fb2c839281f988d505714eea6a0c25a2e30ea2
otwjunior <jamesthebark@proton.me>
2025-08-14 13:50:28 +0300

    implemented filters

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'rest_framework'
+    'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {'DEFAULT_FILTER_BACKENDS':['django_filters.rest_framework.DjangoFilterBackend']}
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,8 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.generics import (ListAPIView, RetrieveAPIView,CreateAPIView,UpdateAPIView,DestroyAPIView)
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 """implemting custom views for book model"""
 #list all books
 class BookListView(ListAPIView):
@@ -10,12 +12,22 @@ class BookListView(ListAPIView):
     serializer_class = BookSerializer
     #onlyauthenticated user can view book catalog
     permission_classes= [IsAuthenticatedOrReadOnly]
+    #filtering displayed book details
+    filter_backends =[DjangoFilterBackend,filters.SearchFilter,filters.OrderingFilter]
+    filterset_fields = ['title','author','publication_year']
+    #ordering  by title and publication_year
+    ordering_fields= ['title', 'publication_year']
+    #filtering by search
+    search_fields =['title','author__name']
 #view single book
 class BookDetailView(RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     #onlyauthenticated user can request a book
     permission_classes= [IsAuthenticatedOrReadOnly]
+    #implemeting search filters
+    filter_backends =[filters.SearchFilter]
+    search_fields =['title','author']
 #add a book
 class BookCreateView(CreateAPIView):
     queryset = Book.objects.all()
