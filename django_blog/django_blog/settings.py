0a5410490055ca9a7e8f94607e3e04d302fd2e25
otwjunior <jamesthebark@proton.me>
2025-08-21 16:18:24 +0300

    styled the teplates

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    
 ]
 
 MIDDLEWARE = [
@@ -127,4 +128,4 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
